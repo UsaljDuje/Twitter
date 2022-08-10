ea2c70f13b9acd1bfdd171e51bedd54de8a1ed39
Duje <duje@Lukas-MacBook-Air.local>
2022-08-10 10:18:48 +0200

    gotov2

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAncomsXi2tZc4B6vIp9Akyl0EdRbBN75I",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "twitter-v4-9e7d8.firebaseapp.com",
   projectId: "twitter-v4-9e7d8",
   storageBucket: "twitter-v4-9e7d8.appspot.com",
